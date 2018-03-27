cc9d47177a4cd14b726ac65f237454347610c94d
Ibrahim Beqiri <ibraa.cs@msn.com>
2018-03-27 19:08:32 +0200

    fixes

--- public/javascripts/speech.js
+++ public/javascripts/speech.js
@@ -1,8 +1,12 @@
 (function($){
 
     var $recordButton = $('#main-record-button');
-    var $resultText = $('#result-text');
+   // var $resultText = $('#result-text');
     var transcript;
+    var interimResult = '';
+    var textArea = $('#result-text');
+    var textAreaID = 'result-text';
+
     if('webkitSpeechRecognition' in window )
     {
         var recognition = new webkitSpeechRecognition();
@@ -14,6 +18,7 @@
             if($(this).hasClass('stopped')){
                 console.log("started!");
                 recognition.start();
+                textArea.focus();
                 $recordButton.addClass('pulse');
                 $recordButton.addClass('red');
                 $recordButton.addClass('darken-1');
@@ -28,19 +33,35 @@
             }
 
         })
-        
+
         recognition.onresult = function (event) {
+            var pos = textArea.getCursorPosition() - interimResult.length;
+            textArea.val(textArea.val().replace(interimResult, ''));
+            interimResult = '';
+            textArea.setCursorPosition(pos + '');
+            for (var i = event.resultIndex; i < event.results.length; ++i) {
+                if (event.results[i].isFinal) {
+                    insertAtCaret(textAreaID, event.results[i][0].transcript);
+                    transcript = event.results[i][0].transcript;
+                } else {
+                    isFinished = false;
+                    insertAtCaret(textAreaID, event.results[i][0].transcript + '\u200B');
+                    interimResult += event.results[i][0].transcript + '\u200B';
+                    transcript = interimResult;
+                }
+            }
+        };
+        /*recognition.onresult = function (event) {
             $resultText.text('');
             for(var i = event.resultIndex; i < event.results.length; ++i)
             {
-                //recognition.stop();
+                //recognition.stop()
                 transcript = event.results[i][0].transcript;
                 $resultText.text($resultText.text() + transcript);
 
             }
 
-
-        }
+        }*/
         recognition.onend = function (event) {
             $recordButton.removeClass('pulse');
             $recordButton.removeClass('red');
@@ -49,11 +70,10 @@
             //msg.lang = 'en-US';
             speechSynthesis.speak(msg);
         }
-
     }
     else
     {
         $recordButton.hide();
     }
-
 })(jQuery);
+
